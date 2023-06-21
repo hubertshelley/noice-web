2c73cea80d0cc43ca6e6a6be73367cc2a1486264
hubertshelley <hubertshelley@163.com>
2023-06-21 10:46:14 +0800

    密码可为空

--- migrations/20230527075745_noice-web-user.sql
+++ migrations/20230527075745_noice-web-user.sql
@@ -3,7 +3,7 @@ CREATE TABLE `noice_db`.`noice_web_user`
 (
     `id`       bigint       NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `username` varchar(36)  NOT NULL UNIQUE COMMENT '用户名',
-    `password` varchar(255) NOT NULL COMMENT '密码',
+    `password` varchar(255) NULL COMMENT '密码',
     `name`     varchar(36)  NULL COMMENT '用户名称',
     PRIMARY KEY (`id`)
 ) COMMENT = '用户表';
